8efe18fc7edbabce9d1f18c28e79f24baafb57ff
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-16 01:50:26 -0400

    added Base

---
+++ src/main/java/Sheep/Wars/Main.java
@@ -0,0 +1,10 @@
+package Sheep.Wars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import kr.entree.spigradle.annotations.SpigotPlugin;
+ 
+@SpigotPlugin
+public class Main extends JavaPlugin{
+    
+}
