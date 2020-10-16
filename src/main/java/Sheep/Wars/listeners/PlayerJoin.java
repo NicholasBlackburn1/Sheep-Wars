467454846ac0c10bb682c875befb791689023893
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-16 06:26:47 -0400

    Added listeners

--- src/main/java/Sheep/Wars/listeners/PlayerJoin.java
+++ src/main/java/Sheep/Wars/listeners/PlayerJoin.java
@@ -1,6 +1,13 @@
 package Sheep.Wars.listeners;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
  * this is an Player Join Lisener
@@ -9,4 +16,32 @@ import org.bukkit.event.Listener;
  */
 public class PlayerJoin implements Listener{
     
+    //Handles player joining the game
+    @EventHandler
+    public void playerJoin(PlayerJoinEvent event){
+
+    }
+
+    // Handle Player Leaving the game 
+    @EventHandler
+    public void playerLeaves(PlayerQuitEvent event){
+
+    }
+
+    // removes idle people from the game
+    @EventHandler
+    public void playerKick(PlayerKickEvent event){
+
+    }
+
+    @EventHandler
+    public void playerDamage(EntityDamageEvent event, Entity entity){
+        entity = event.getEntity();
+        
+        if(entity instanceof Player) {
+            Player player = (Player)entity;
+            
+        }
+       
+    }
 }
