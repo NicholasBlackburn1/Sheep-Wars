66fb1f134d688b1fd91dd985f31bf69ed77227d8
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-16 06:30:18 -0400

    added the player damage listener

--- src/main/java/Sheep/Wars/listeners/PlayerJoin.java
+++ src/main/java/Sheep/Wars/listeners/PlayerJoin.java
@@ -1,5 +1,6 @@
 package Sheep.Wars.listeners;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +9,8 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import Sheep.Wars.Main;
 
 /**
  * this is an Player Join Lisener
@@ -37,9 +40,17 @@ public class PlayerJoin implements Listener{
     @EventHandler
     public void playerDamage(EntityDamageEvent event, Entity entity){
         entity = event.getEntity();
-        
+
         if(entity instanceof Player) {
             Player player = (Player)entity;
+            Main.log.warning(Main.loggerPreFix + "Players heath"+player.getHealth() +player.getName());
+
+            if(player.getHealth() < 2){
+                event.setCancelled(true);
+                player.setHealth(10);
+                player.setGameMode(GameMode.SPECTATOR);
+                Main.log.warning(Main.loggerPreFix+" "+player.getName() + " "+ player.getGameMode().toString());
+            }
             
         }
        
