893af30ea7d650f8269959c66460ec8c59edf403
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-10-17 01:07:48 -0400

    trying to get Death message to broadcast

--- src/main/java/Sheep/Wars/listeners/PlayerJoin.java
+++ src/main/java/Sheep/Wars/listeners/PlayerJoin.java
@@ -6,10 +6,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
@@ -24,7 +26,6 @@ import Sheep.Wars.Main;
  */
 public class PlayerJoin implements Listener{
 
-   
     
     //Handles player joining the game
     @EventHandler
@@ -47,13 +48,13 @@ public class PlayerJoin implements Listener{
     // Handle Player Leaving the game 
     @EventHandler
     public void playerLeaves(PlayerQuitEvent event){
-        event.setQuitMessage(ChatColor.YELLOW + event.getPlayer().getName()+" ");
+        event.setQuitMessage(ChatColor.YELLOW + event.getPlayer().getName()+ChatColor.RED+"DISCONNECTED");
     }
 
     // removes idle people from the game
     @EventHandler
     public void playerKick(PlayerKickEvent event){
-        
+     
     }
 
     /**
@@ -70,14 +71,15 @@ public class PlayerJoin implements Listener{
 
             Main.log.warning(Main.loggerPreFix + "Players heath"+player.getHealth() +player.getName());
 
-            if(player.getHealth() <= 2){
+            if(player.getHealth() <= 2.5){
+                Bukkit.broadcast(ChatColor.GREEN + player.getLastDamageCause().getEntity().getName() + ChatColor.YELLOW + "DeathMessage",null);
                 player.setHealth(10);
                 player.setGameMode(GameMode.SPECTATOR);
                 Main.log.warning(Main.loggerPreFix+" "+player.getName() + " "+ player.getGameMode().toString());
-            if(){ 
-                player.sendTitle(ChatColor.RED+"U Died".toUpperCase(), ChatColor.GREEN+"Respawn in" +" "+ 1);
             }
-            
+            /*if(){ 
+                player.sendTitle(ChatColor.RED+"U Died".toUpperCase(), ChatColor.GREEN+"Respawn in" +" "+ 1);*/
+
             else{
                 Main.log.warning(Main.loggerPreFix+" "+player.getName()+ "IS not dead but hurt\n");
                 
@@ -86,4 +88,5 @@ public class PlayerJoin implements Listener{
         }
        
     }
+    
 }
